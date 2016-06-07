e8411a623b4b216f62da22a00d64cf26c6b393ba
Edu <eduardoferrandezr@gmail.com>
2016-06-07 19:52:31 +0200

    some minor fixes

--- wordcount.py
+++ wordcount.py
@@ -20,6 +20,9 @@ class WordCount(db.Model):
 
 
 def remove_html_tags(text):
+    """
+        Removes all the html tags, scripts and styles
+    """
     # remove not visible content
     soup = BeautifulSoup(text, 'html.parser')
     texts = soup.findAll(text=True)
@@ -38,10 +41,17 @@ def remove_html_tags(text):
 
 
 def extract_words(text):
+    """
+        Returns a list with the words of the text
+    """
     return re.compile('\w+').findall(text)
 
 
 def count_words(words):
+    """
+        Receives a list of words and returns a dictionary with the word and its ocurrences in the list
+    """
+
     worddict = dict()
     for word in words:
         word = word.lower()
@@ -52,12 +62,19 @@ def count_words(words):
 
 
 def top100words(worddict):
+    """
+        Returns a list of tuples with the 100 most frequent words
+    """
     sortedentries = sorted(worddict.items(), key=operator.itemgetter(1))
     # we take the last 100 entries
     return sortedentries[-100:]
 
 
 def url_wordcount(url):
+    """
+        Receives an url and returns a tuple with the 100 most frequent words
+    """
+
     f = urllib.urlopen(url)
     text = f.read()
     text = remove_html_tags(text)
@@ -68,25 +85,44 @@ def url_wordcount(url):
 
 
 def build_wordcloud(wordcount):
+    """
+        Receives a list of tuples (word, count) and returns a dictionary of word and category
+        where category is a value from 1 to 10 where 1 means the least frequent and 10 means the most frequent
+    """
+
     wordcloud = dict()
     max_count = wordcount[-1][1]
     for word, count in wordcount:
+        # size goes from 1 to 10 and divides the frequencies in categories
         size = int(math.ceil(10 * count/max_count))
         wordcloud[word] = size
     return wordcloud
 
 
+def hash_word(word):
+    """
+        Computes the salted hash for the word
+    """
+
+    m = hashlib.sha1()
+    m.update(settings.get("salt"))
+    m.update(word)
+    return m.hexdigest()
+
+
 def add_to_database(wordcount):
+    """
+        Adds all the words to the database, with its frequency. If the word doesn't exists, it is inserted
+        with its frequency. If the word already exists (its hash matches with a stored one) then the count
+        is added to the stored frequency and the row is updated.
+    """
     for word, count in wordcount:
-        m = hashlib.sha1()
-        m.update(settings.get("salt"))
-        m.update(word)
-        id = m.hexdigest()
+        id = hash_word(word)
         obj = db.Query(WordCount).filter("id =", id).get()
         if obj is None:
             obj = WordCount(
                 id=id,
-                word=word,
+                word=word,  # TODO: encrypt with public key
                 count=count
             )
         else:
@@ -94,17 +130,21 @@ def add_to_database(wordcount):
         obj.put()
 
 
-
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
+        wordcloud = {}
+        error = None
         url = self.get_argument("url", None)
         if url is not None:
-            wordcount = url_wordcount(url)
-            add_to_database(wordcount)
-            wordcloud = build_wordcloud(wordcount)
-        else:
-            wordcloud = {}
-        self.render("main.html", url=url, wordcloud=wordcloud)
+            try:
+                wordcount = url_wordcount(url)
+                add_to_database(wordcount)
+                wordcloud = build_wordcloud(wordcount)
+            except IOError:
+                error = "The url seems to be wrong!"
+
+        self.render("main.html", url=url, wordcloud=wordcloud, error=error)
+
 
 class AdminHandler(tornado.web.RequestHandler):
     def get(self):
